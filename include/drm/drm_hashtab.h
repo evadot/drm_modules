1de5eb2ad680f3136cefca47dee9809fcedd34df
Emmanuel Vadot <manu@freebsd.org>
2017-05-08 11:59:34 +0200

    Sync drm_hashtab.h with linux 3.8.13

--- include/drm/drm_hashtab.h
+++ include/drm/drm_hashtab.h
@@ -25,7 +25,6 @@
  *
  *
  **************************************************************************/
-
 /*
  * Simple open hash tab implementation.
  *
@@ -43,15 +42,24 @@
 #define drm_hash_entry(_ptr, _type, _member) container_of(_ptr, _type, _member)
 
 struct drm_hash_item {
+#ifdef __linux__
+	struct hlist_node head;
+#elif __FreeBSD__
 	LIST_ENTRY(drm_hash_item) head;
+#endif
 	unsigned long key;
 };
 
 struct drm_open_hash {
+#ifdef __linux__
+	struct hlist_head *table;
+	u8 order;
+#elif __FreeBSD__
 	LIST_HEAD(drm_hash_item_list, drm_hash_item) *table;
 	unsigned int size;
 	unsigned int order;
 	unsigned long mask;
+#endif
 };
 
 extern int drm_ht_create(struct drm_open_hash *ht, unsigned int order);
@@ -65,5 +73,22 @@ extern void drm_ht_verbose_list(struct drm_open_hash *ht, unsigned long key);
 extern int drm_ht_remove_key(struct drm_open_hash *ht, unsigned long key);
 extern int drm_ht_remove_item(struct drm_open_hash *ht, struct drm_hash_item *item);
 extern void drm_ht_remove(struct drm_open_hash *ht);
+
+#ifdef __linux__
+/*
+ * RCU-safe interface
+ *
+ * The user of this API needs to make sure that two or more instances of the
+ * hash table manipulation functions are never run simultaneously.
+ * The lookup function drm_ht_find_item_rcu may, however, run simultaneously
+ * with any of the manipulation functions as long as it's called from within
+ * an RCU read-locked section.
+ */
+#define drm_ht_insert_item_rcu drm_ht_insert_item
+#define drm_ht_just_insert_please_rcu drm_ht_just_insert_please
+#define drm_ht_remove_key_rcu drm_ht_remove_key
+#define drm_ht_remove_item_rcu drm_ht_remove_item
+#define drm_ht_find_item_rcu drm_ht_find_item
+#endif
 
 #endif
